59a71ec5f09971f425ba38e06ff06b09d8ce805b
vinay8892 <37689515+vinay8892@users.noreply.github.com>
2024-02-11 23:35:07 +0530

    Delete js directory

--- js/5600-js-fixed-form.js
+++
@@ -1,5 +0,0 @@
-$("#fixed-form-container .body").hide();
-$("#fixed-form-container .button").click(function () {
-        $(this).next("#fixed-form-container div").slideToggle(400);
-        $(this).toggleClass("expanded");
-    });
