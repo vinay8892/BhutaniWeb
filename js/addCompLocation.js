bfcd2b22d1dfec673b6798c8ccd65de792d4c435
vinay8892 <37689515+vinay8892@users.noreply.github.com>
2024-02-12 11:54:17 +0400

    Add files via upload

--- js/addCompLocation.js
+++ js/addCompLocation.js
@@ -13,7 +13,7 @@
 firebase.initializeApp(config);
 //create firebase database reference
 var dbRef = firebase.database();
-var compRef = dbRef.ref('compInfo_OneFng');
+var compRef = dbRef.ref('compInfo_Group108');
 var LeadsLogRef = dbRef.ref('LeadsLog');
 
 //save contact
